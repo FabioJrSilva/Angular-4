7ad75b8d6f45643d8f94bb578d4c399a4515cab9
Fabio Jr Silva <fabiopt6@gmail.com>
2017-09-14 20:40:52 -0300

    Criando um Mock de objetos Frase

---
+++ app1/src/app/painel/frase.mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model'
+
+const FRASE: Frase[] = [
+    {fraseEng:'I like learn', frasePtBr:'Eu gosto de aprender'},
+    {fraseEng:'I watch tv', frasePtBr:'Eu assisto tv'},
+    {fraseEng:'I eat bread', frasePtBr:'Eu como pão'},
+    {fraseEng:'How are you?', frasePtBr:'Como vai voçê?'}
+]
---
+++ app1/src/app/shared/frase.model.ts
@@ -0,0 +1,5 @@
+export class Frase{
+    //public fraseEng: string; criado direto no construtor
+    //public frasePtBr: string; criado direto no construtor
+    constructor(public fraseEng: string, public frasePtBr: string){ }
+}
